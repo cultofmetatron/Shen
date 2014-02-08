e7eb4c8b13d8379dc6996b068db9927df427a23f
Peter de Croos <cultofmetatron@aumlogic.com>
2014-02-07 19:17:59 -0800

    dispatch works with first test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 module.exports = {
-  co : require('./lib/co.js'),
-  cascade: require('./lib/cascade.js'),
-  branch: require('./lib/branch.js'),
-  delay: require('./lib/delay.js')
+  co        : require('./lib/co.js'),
+  cascade   : require('./lib/cascade.js'),
+  branch    : require('./lib/branch.js'),
+  delay     : require('./lib/delay.js'),
+  dispatch  : require('./lib/dispatch.js')
 };
--- test/dispatch-test.js
+++ test/dispatch-test.js
@@ -2,9 +2,30 @@ var should  = require('should');
 var fuu     = require('../index.js');
 var co      = require('co');
 var Promise = require('bluebird');
+var _       = require('underscore');
+var utils   = require('../lib/utils.js');
+
 
 describe('Dispatch', function() {
 
+  it('should dispatch to a branch', function(done) {
+    
+    var genFunc = fuu.dispatch(function *(paths) {
+      var foo =  yield paths['fuu'];
+      return foo;
+    }, {
+      fuu: function *() {
+        return 'yeaaaa';
+      }
+    });
+
+    co(genFunc)(function(err, value) {
+      value.should.equal('yeaaaa');
+      done();
+    });
+
+
+  });
 
 
 });
