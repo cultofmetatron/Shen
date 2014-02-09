465c99154564945556d2ed765a89fe7b94ae3b19
Peter de Croos <cultofmetatron@aumlogic.com>
2014-02-09 02:12:38 -0800

    added a test server for koa tests

--- test/servers/koa-cascade.js
+++ test/servers/koa-cascade.js
@@ -1,1 +1,47 @@
-var koa = 
+var koa = require('koa');
+var app = koa();
+
+var shen = require('../../index.js');
+
+app.use(function *(next) {
+  this.status = 200;
+  this.body = yield next;
+});
+
+var foo = shen.cascade(
+  function *(next) {
+    return yield next;
+  },
+  shen.branch(function *( path1, path2) {
+      return yield path2;
+    },
+    function *(next) {
+      return yield next;
+    },
+    shen.cascade(
+      function *(next) {
+        return yield next;
+      },
+      function *(next) {
+        return yield next;
+      },
+      function *(next) {
+        return yield next;
+      })),
+ function *(next) {
+    return yield next;
+ });
+
+app.use(foo);
+
+
+app.use(function *() {
+  return "fuu";
+});
+
+
+app.listen(3000);
+
+
+
+
