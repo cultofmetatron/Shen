5dff999db29fcf478f8b9c64e824ef7fcaf3f7d5
Peter de Croos <cultofmetatron@aumlogic.com>
2014-02-08 18:04:35 -0800

    added secnd oscillator test

--- test/oscillator-test.js
+++ test/oscillator-test.js
@@ -7,26 +7,39 @@ var utils   = require('../lib/utils.js');
 
 describe('Oscillator', function() {
   it('should run run the function over and over', function(done) {
-    this.timeout(10000)
+    this.timeout(10000);
     var i = 0;
     var test = function() { i++; };
 
     var genFunc = shen.oscillator(function *() {
-      console.log('running');
       test();
     }, 500);
 
     co(genFunc)();
 
     setTimeout(function() {
-      console.log('this is i: ', i);
       i.should.be.greaterThan(5);
       done();
     }, 6000);
-
-
-  
   });
+
+  xit('should let me stop the oscillation with stopTick()', function(done) {
+    this.timeout(10000);
+    var i = 0;
+    var test = function() { i++; };
+
+    var genFunc = shen.oscillator(function *() {
+      test();
+    }, 500);
+
+    co(genFunc)();
+
+    setTimeout(function(err, val) {
+      i.should.be.greaterThan(5);
+      done();
+    }, 6000);
+  });
+
 
 
 
